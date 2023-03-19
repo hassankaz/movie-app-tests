a5f69c9b61a00f968d2a25a238634406d09e59bb
hassankaz <hassankaz@gmail.com>
2023-03-19 12:37:06 -0700

    updated the sample test file

--- cypress/e2e/sample_test.cy.js
+++ cypress/e2e/sample_test.cy.js
@@ -1,6 +1,5 @@
-describe('sample test', () => {
-it('check header text', ()=>{
-    cy.visit("https://www.creativelive.com/");
-    cy.get('h1').contains('Do what you love.').should('be.visible');
-})
-});
+describe('My First Test', () => {
+    it('Visits the Kitchen Sink', () => {
+      cy.visit('https://example.cypress.io')
+    })
+  })
